68dc2e128e425278cb7ac90724ed40bd9e05cee9
RenaatST <stuijkrenaat@hotmail.com>
2014-12-02 10:44:37 +0100

    link

--- view/whiteboards/detail.php
+++ view/whiteboards/detail.php
@@ -1,18 +1,21 @@
 <section id="whiteboardDetail">
 <header><h2><?php echo $whiteboard["title"];?></h2></header>
-<h5><?php echo "created on ".$whiteboard["date_added"]; ?></h5>
-<button type="button" id="addUserbtn" class="btn btn-sm btn-success"><a href="#modal">Add user</a></button>
-<div id="modal" class='selectoverlay hidden'>
-	<div>
-		<a href="#close" title="Close" class="close">X</a>
-		<form>
-			<ul>
-			<?php foreach ($users as $user) {
-				echo "<li><input class=\"userinput\" type=\"checkbox\" name=\"user\" value=\"{$user['username']}\">{$user['username']}</li>";
-			} ?>
-			</ul>
-		</form>
-	</div>
+<h5><?php echo "created on ".$whiteboard["date_added"]; ?></h3>
+<button type="button" id="addUserbtn" class="btn btn-sm btn-success">Add user</button>
+<?php
+		echo "<a href='index.php?page=canvaspage&boardid={$_GET['id']}'>Go to canvas</a>";
+?>
+
+<div class='selectoverlay hidden'>
+	<button type='button' class='btn btn-xs btn-danger closebtn'>close</button>	
+	<form>
+		<ul>
+		<?php foreach ($users as $user) {
+			echo "<li><input class=\"userinput\" type=\"checkbox\" name=\"user\" value=\"{$user['username']}\">{$user['username']}</li>";
+		} ?>
+		</ul>
+	</form>
+
 </div>
 
 </section>
