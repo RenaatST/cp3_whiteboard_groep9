e55cdc79409a26510a90ae4712c6e31d631e22b4
gillesplatteuw <gilles.platteeuw@student.howest.be>
2014-12-02 09:55:16 +0100

    update voor renaat

--- view/whiteboards/detail.php
+++ view/whiteboards/detail.php
@@ -1,17 +1,18 @@
 <section id="whiteboardDetail">
 <header><h2><?php echo $whiteboard["title"];?></h2></header>
-<h5><?php echo "created on ".$whiteboard["date_added"]; ?></h3>
-<button type="button" id="addUserbtn" class="btn btn-sm btn-success">Add user</button>
-<div class='selectoverlay hidden'>
-	<button type='button' class='btn btn-xs btn-danger closebtn'>close</button>	
-	<form>
-		<ul>
-		<?php foreach ($users as $user) {
-			echo "<li><input class=\"userinput\" type=\"checkbox\" name=\"user\" value=\"{$user['username']}\">{$user['username']}</li>";
-		} ?>
-		</ul>
-	</form>
-
+<h5><?php echo "created on ".$whiteboard["date_added"]; ?></h5>
+<button type="button" id="addUserbtn" class="btn btn-sm btn-success"><a href="#modal">Add user</a></button>
+<div id="modal" class='selectoverlay hidden'>
+	<div>
+		<a href="#close" title="Close" class="close">X</a>
+		<form>
+			<ul>
+			<?php foreach ($users as $user) {
+				echo "<li><input class=\"userinput\" type=\"checkbox\" name=\"user\" value=\"{$user['username']}\">{$user['username']}</li>";
+			} ?>
+			</ul>
+		</form>
+	</div>
 </div>
 
 </section>
